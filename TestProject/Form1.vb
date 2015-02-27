fb3be21a6aa6c63c1f95319193d4d404182b0d7a
Artit Kiuwilai <first087@gmail.com>
2015-02-27 16:44:56 +0700

    Finish Button1.Click on Branch "Dev"

--- TestProject/Form1.vb
+++ TestProject/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
+        Console.WriteLine("http://www.artit-k.com")
     End Sub
 End Class
